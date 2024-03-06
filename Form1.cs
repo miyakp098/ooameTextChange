77d9c7f410bec7d67be335f422a38b852c901c9d
miya_kp <ybtkeppen813@gmail.com>
2024-03-06 16:35:10 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ooameTextChange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePass = $@"{textBox1.Text}\{textBox2.Text}.txt";
+            string outputFile = $@"{textBox1.Text}\{textBox2.Text}_shusei.txt"; // 結果を保存するファイルのパス
+
+            // ファイルの存在チェック
+            if (!File.Exists(filePass))
+            {
+                MessageBox.Show("指定されたファイルが見つかりません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var lines = File.ReadAllLines(filePass);
+                using (StreamWriter sw = new StreamWriter(outputFile, false)) // false でファイルを新規作成または上書き
+                {
+                    foreach (var line in lines)
+                    {
+                        // 「.」の前後の空白を削除
+                        var modifiedLine = Regex.Replace(line, @"\s*\.\s*", ".");
+                        // アルファベットの後ろにある空白を削除（アルファベットの前の空白は削除しない）
+                        modifiedLine = Regex.Replace(modifiedLine, @"([a-zA-Z])\s+", "$1");
+                        // 残りの空白をすべてコンマに置き換える
+                        modifiedLine = Regex.Replace(modifiedLine, @"\s+", ",");
+                        sw.WriteLine(modifiedLine); // 修正した行を書き込む
+                    }
+                }
+                MessageBox.Show($"ファイルが正常に保存されました: {outputFile}", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ファイルの処理中にエラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("ooameTextChange")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ooameTextChange")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("d196d422-1a59-4b77-92ab-5366ec70c5fd")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
